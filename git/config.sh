5acc5faa00063c902b690968a54151bb2bd65a95
Andrey Shpak <ashpak@ashpak.ru>
2016-04-04 16:33:47 +0000

    Added settings display.

--- git/config.sh
+++ git/config.sh
@@ -3,3 +3,7 @@ git config --global user.name "Andrey Shpak"
 git config --global user.email ashpak@ashpak.ru
 git config --global core.editor nano
 
+echo "New username is:"
+git config --global user.name
+echo "New e-mail is:"
+git config --global user.email
